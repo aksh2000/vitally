7f2e33d9c0f02e665b0e9bdd68cad8a458065837
aksh2000 <akshithchittiveli@gmail.com>
2020-12-22 02:31:03 +0530

    App Level Backend Restructuring

--- lib/backend/backend.dart
+++ lib/backend/backend.dart
@@ -1,30 +1,7 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:vitally/dataModels/backendResponse.dart';
-import 'package:vitally/dataModels/user.dart';
+import 'package:vitally/backend/handlers/account.dart';
 
 class Backend {
-  //This class is responsible for firestore data writing / updating
-
-  Future<Response> createAccount(User user,
-      [int count = 0, String error = ""]) async {
-    if (count <= 5) {
-      try {
-        await FirebaseFirestore.instance
-            .collection(user.userId)
-            .doc("userData")
-            .set(user.userDetailsForRegistration);
-        return Response(
-            success: true,
-            error: "none",
-            data: {"message": "Account Created Successfully"});
-      } catch (e) {
-        return await createAccount(user, count++, e.toString());
-      }
-    } else {
-      return Response(
-          success: false,
-          error: error,
-          data: {"message": "Failed to Create Account"});
-    }
-  }
+  //  This class is responsible for firestore data writing / updating
+  //  Every method of the following classes are expected to return a Response object
+  final AccountHandler accountHandler = AccountHandler();
 }
---
+++ lib/backend/handlers/account.dart
@@ -0,0 +1,28 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:vitally/dataModels/backendResponse.dart';
+import 'package:vitally/dataModels/user.dart';
+
+class AccountHandler {
+  Future<Response> createAccount(User user,
+      [int count = 0, String error = ""]) async {
+    if (count <= 5) {
+      try {
+        await FirebaseFirestore.instance
+            .collection(user.userId)
+            .doc("userData")
+            .set(user.userDetailsForRegistration);
+        return Response(
+            success: true,
+            error: "none",
+            data: {"message": "Account Created Successfully"});
+      } catch (e) {
+        return await createAccount(user, count++, e.toString());
+      }
+    } else {
+      return Response(
+          success: false,
+          error: error,
+          data: {"message": "Failed to Create Account"});
+    }
+  }
+}
